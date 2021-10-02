2fd1e1420d1b5092a095029f7197046ea4f6b595
Sarav <saravanant@signeasy.com>
2021-10-03 05:12:46 +0530

    Version1-Functional

--- main.js
+++ main.js
@@ -1,14 +1,20 @@
 // Modules to control application life and create native browser window
-const {app, BrowserWindow} = require('electron')
+const {app, BrowserWindow, ipcMain} = require('electron')
 const path = require('path')
+const AWS = require("@aws-sdk/client-sqs");
+const { GetQueueUrlResult } = require('@aws-sdk/client-sqs');
+let mainWindow;
+let QURL;
 
 function createWindow () {
   // Create the browser window.
-  const mainWindow = new BrowserWindow({
+  mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
-      preload: path.join(__dirname, 'preload.js')
+      // preload: path.join(__dirname, 'preload.js'),
+      nodeIntegration: true,
+      contextIsolation: false,
     }
   })
 
@@ -37,6 +43,69 @@ app.whenReady().then(() => {
 // explicitly with Cmd + Q.
 app.on('window-all-closed', function () {
   if (process.platform !== 'darwin') app.quit()
+})
+
+
+ipcMain.on("listqueue",function(event, msgno, sqsqueue){
+  console.log("Messages",msgno)
+  console.log("SQSQueue",sqsqueue.replace(/\s+/g,' ').trim())
+  //Performaing AWS SQS Task
+  const sqs = new AWS.SQS({ region: "us-east-1" });
+
+
+    var queuename = sqsqueue.replace(/\s+/g,' ').trim();
+    
+
+    
+    // First we need to get the URL based on the Name
+    
+    var params = {
+      QueueName: queuename
+    }
+    sqs.getQueueUrl(params, function(err, data) {
+      if (err) console.log(err, err.stack); // an error occurred
+      else{
+        console.log(data.QueueUrl); // successful response
+        QURL=data.QueueUrl
+          // Once the URL is ready we can get messages
+          var para = {
+              AttributeNames: 
+              [
+                "All"
+              ],
+              MaxNumberOfMessages: msgno,
+              MessageAttributeNames: 
+              [
+                "All"
+              ],
+              QueueUrl: QURL
+            };
+
+            console.log("ParamsToSQS"+JSON.stringify(para))
+            sqs.receiveMessage(para, function(err, data) {
+              if (err) 
+              {
+                console.log(err, err.stack); // an error occurred
+                mainWindow.webContents.send('listqueue', err); // Send the response to the renderer
+              }
+              else {
+                console.log(data)
+                mainWindow.webContents.send('listqueue', data.Messages); // Send the response to the renderer
+              }     
+            });
+          }  
+        });
+
+})
+
+ipcMain.on('asynchronous-message', (event, arg) => {
+  console.log(arg) // prints "ping"
+  event.reply('asynchronous-reply', 'pong')
+})
+
+ipcMain.on('synchronous-message', (event, arg) => {
+  console.log(arg) // prints "ping"
+  event.returnValue = 'pong'
 })
 
 // In this file you can include the rest of your app's specific main process
--- renderer.js
+++ renderer.js
@@ -4,3 +4,48 @@
 // `nodeIntegration` is turned off. Use `preload.js` to
 // selectively enable features needed in the rendering
 // process.
+
+const ipc = require('electron').ipcRenderer
+
+const outbox = document.getElementById("outbox")
+
+var outputdata = [];
+
+var stringbuffer = "";
+
+const form = document.querySelector('form')
+form.addEventListener('submit',submitform);
+
+function submitform(e){
+  e.preventDefault();
+  const msgno = document.querySelector("#msgno").value
+  const sqsqueue = document.querySelector("#sqsqueue").value
+  console.log("=>",msgno,"=>",sqsqueue)
+  ipc.send('listqueue', msgno, sqsqueue)  
+}
+
+ipc.on('listqueue', function(event, data){
+    console.log("data return",data.join("\n"))
+    
+
+    data.forEach((e, index) => {
+        msgcount=index++
+        stringbuffer+="\n-----------------------------------"
+        stringbuffer+="\nMessage Number => "+msgcount
+        stringbuffer+="\n-----------------------------------\n"
+        outputdata.push(JSON.parse(e.Body).Message);
+        stringbuffer+=JSON.stringify(outputdata);
+        stringbuffer+="\n-----------------------------------"
+        stringbuffer+="\n\n"
+    });
+    
+    outbox.innerHTML=stringbuffer 
+})
+
+console.log(ipc.sendSync('synchronous-message', 'ping')) // prints "pong"
+
+ipc.on('asynchronous-reply', (event, arg) => {
+  console.log(arg) // prints "pong"
+})
+ipc.send('asynchronous-message', 'ping')
+
